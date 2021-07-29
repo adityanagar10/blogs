7571adca9ed764e3e60af8d804845454fe76cc57
Aditya Nagar <nagar.s.aditya@gmail.com>
2021-07-29 22:53:41 +0530

    Homepage updated and post component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { HomePage } from './pages/homepage/homepage'
+import {Post} from './components/post/post'
+import {TitleBar} from './components/titlebar/titlebar'
 
 function App() {
   return (
     <div className="App">
-    <HomePage />    
+    <TitleBar />
+     <HomePage />
     </div>
   );
 }
--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import './homepage.scss'
-import { TitleBar } from '../../components/titlebar/titlebar'
 
 export const HomePage = () => {
     return(
         <>
-        <TitleBar />
         <div className="homepageTitle">
         <p>
             <span className="title">Welcome to my blog!</span>
-            <button className="learnFromMe"> Learn From Me! </button>
+            <button className="learnFromMe"> Learn With Me! </button>
         </p>
         </div>
         </>
